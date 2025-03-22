9ad6d350220a3b80eee41200c80a273ddadcb602
MohamedMamdouh-432 <mohammedmamdouh432@gmail.com>
2025-03-22 17:35:19 +0200

    update nodejs methodology: to add newline after <p> tag

--- extract_html.js
+++ extract_html.js
@@ -1,17 +1,21 @@
-const fs = require('fs').promises;
 const { JSDOM } = require('jsdom');
 
 function extractText(node) {
     let text = '';
     node.childNodes.forEach((child) => {
-        if (child.nodeType === 3) text += child.textContent.trim() + ' ';
-        else if (
+        if (child.nodeType === 3) {
+            text += child.textContent.trim() + ' ';
+        } else if (
             child.nodeType === 1 &&
             !['STYLE', 'SCRIPT', 'NOSCRIPT', 'LINK', 'META'].includes(
                 child.nodeName
             )
-        )
+        ) {
             text += extractText(child);
+            if (child.nodeName === 'P') {
+                text += '\n';
+            }
+        }
     });
     return text;
 }
@@ -20,12 +24,15 @@ module.exports = async (htmlCode) => {
     try {
         const dom = new JSDOM(htmlCode);
         const document = dom.window.document;
+
         const extractedText = extractText(document.body)
             .replace(/\s+/g, ' ')
+            .replace(/\n\s+/g, '\n')
             .trim();
+
         return extractedText;
     } catch (err) {
-        console.error('Error reading file:', err);
+        console.error('Error processing HTML:', err);
         throw err;
     }
 };
